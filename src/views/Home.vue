34d5e9222582d1c6a07283d2aba2e20b9a032066
Aiden <15000487124@163.com>
2019-08-09 00:14:29 +0800

    初始化部分配置

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="all">
+    <p>11111</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+<style scoped lang="less">
+  .all {
+    p {
+      color: red;
+    }
+  }
+</style>
